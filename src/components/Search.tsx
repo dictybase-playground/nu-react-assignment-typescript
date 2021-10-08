f77eddfd89c8b9263fcdf719cd4b663df1988a43
Sean Kim <seank4@uic.edu>
2021-10-08 16:45:54 -0500

    Attempt to implement search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import { ReducerContext } from '../App';
 
 const Search = () => {
 
     const [searchText, setText] = useState<String | String>("");
+    const { state, dispatch } = useContext(ReducerContext);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setText(event.target.value);
       };
+
+    const createItemData = () => {
+      const new_id = itemList.length + 1 + Math.floor(Math.random() * 9999);
+      dispatch("Add Item", {id: new_id, name: searchText});
+      return; 
+    }
   return (
     <React.Fragment>
       <Box display="flex"
@@ -26,7 +34,7 @@ const Search = () => {
           onChange={handleChange}
           placeholder="Ex: CSS"
         />
-        <Button variant="outlined" color="secondary" /* onClick={handleButtonAdd} */>
+        <Button variant="outlined" color="secondary" onClick={createItemData()} >
           Add Item
         </Button>
       </Box>
