7d89890e93eba99f09f109e618c58becb4857628
Sean Kim <seank4@uic.edu>
2021-10-11 11:56:39 -0500

    Add test case to check textbox typing

--- src/utils/App.test.tsx
+++ src/utils/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+ 
+//   // screen.debug();
+// });
+
+test('Search bar typing & Add Item Button Press', () => {
   render(<App />);
 
-  // screen.debug();
+  const inputComponent = screen.getByRole('textbox');
+  const buttonComponent = screen.getByRole('button', {
+    name: 'Add Item'
+  });
+
+  userEvent.type(inputComponent, 'List Item');
+  expect(inputComponent).toHaveValue('List Item');
+
+  userEvent.click(buttonComponent);
+  expect(inputComponent).toHaveValue('');
 });
--- src/utils/Search.test.tsx
+++ src/utils/Search.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Search from '../components/Search';
 
 test('Button renders with correct text', () => {
     render(<Search />);
 
-    const buttonComponent = screen.getByRole('button');
+    const buttonComponent = screen.getByRole('button', {
+        name: 'Add Item'
+    });
     expect(buttonComponent).toBeInTheDocument();
 
     const typographyText = screen.getByText('Add Item');
@@ -17,4 +20,14 @@ test('Item name input field renders', () => {
 
     const inputComponent = screen.getByRole('textbox');
     expect(inputComponent).toBeInTheDocument();
+})
+
+test('Search Bar typing', () => {
+    render(<Search />);
+
+    const inputComponent = screen.getByRole('textbox');
+
+    userEvent.type(inputComponent, 'List Item');
+    expect(inputComponent).toHaveValue('List Item');
+
 })
